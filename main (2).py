0c5281ca2e2d959fc8161ee4e3e791deccb08d98
swethasangaiya5922128 <144440937+swethasangaiya5922128@users.noreply.github.com>
2023-09-20 15:34:58 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,12 +1,33 @@
-#Implent a recursive function to calculate the factorial of a given number.
+'''implement a class called Bank account that represent a bank account.the class should have private attributes for account number, account holder name,and account balance.include methods to deposit money,withdraw money,and display the account balance.Ensure that the account balance cannot be accessed directly from outside the class.write the program to create an instance of the bank account class and test the deposit and withdrawal functionality.'''
 
-def fact_rec(n):
- if n==0 or n==1:
-  return 1
- else:
-  return n*fact_rec(n-1)
+class BankAccount:
 
-number =int (input("Enter a value :"))
-res = fact_rec(number)
+  def __init__(self, account_number,account_holder_name,initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
 
-print("The factorial of {} is {}.".format(number,res))
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
+    else:
+      print("Invalid deposit amount.")
+  def withdraw (self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("Withdraw ₹{}. New balance:₹{}". format(amount,self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
+
+  def display_balance(self):
+     print("Account balance for {} (Account #{}): ₹{}". format(self.__account_holder_name,self.__account_number,self.__account_balance))
+
+#create an instance of the Bank account class
+account = BankAccount(account_number="123456789", account_holder_name="Swetha",initial_balance=5000.0)
+
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.withdraw(2000.0)
+account.display_balance()
--- main (3).py
+++ main (3).py
@@ -1,14 +1,33 @@
-#write the program that determines whether a year entered by the user is a leap year or not using if-elif-else statements.
+'''implement a class called Bank account that represent a bank account.the class should have private attributes for account number, account holder name,and account balance.include methods to deposit money,withdraw money,and display the account balance.Ensure that the account balance cannot be accessed directly from outside the class.write the program to create an instance of the bank account class and test the deposit and withdrawal functionality.'''
 
-def isLeapYear(year):
- if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-  return True
- else:
-   return False
+class BankAccount:
 
-year = int(input("Enter a year :"))
+  def __init__(self, account_number,account_holder_name,initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
 
-if isLeapYear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not a leap year.'.format(year))
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
+    else:
+      print("Invalid deposit amount.")
+  def withdraw (self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("Withdraw ₹{}. New balance:₹{}". format(amount,self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
+
+  def display_balance(self):
+     print("Account balance for {} (Account #{}): ₹{}". format(self.__account_holder_name,self.__account_number,self.__account_balance))
+
+#create an instance of the Bank account class
+account = BankAccount(account_number="123456789", account_holder_name="Swetha",initial_balance=5000.0)
+
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.withdraw(2000.0)
+account.display_balance()
